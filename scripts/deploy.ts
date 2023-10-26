78e2536f331f58b7175e347d13da59a1f100da3f
sumbatx15 <sumbat15@gmail.com>
2023-10-26 11:26:26 +0300

    changed deploy logic

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,14 @@
 import { ethers } from "hardhat";
 import fs from "fs";
+import path from "path";
 import { getAbi } from "./utils";
+const { exec } = require("child_process");
 
 const SUB_CONTRACT_NAME = "SubscriptionService";
 const BPAY_CONTRACT_NAME = "BPayMoney";
 
 async function main() {
-  const [owner] = await ethers.getSigners();
+  const [owner, ...users] = await ethers.getSigners();
 
   const subscriptionService = await ethers
     .deployContract(SUB_CONTRACT_NAME, [owner.address])
@@ -19,27 +21,49 @@ async function main() {
   const subscriptionServiceAbi = getAbi(SUB_CONTRACT_NAME);
   const bpayAbi = getAbi(BPAY_CONTRACT_NAME);
 
-  // C:/Users/Sumba/OneDrive/Desktop/BPay/bpay/src/abi
+  const frontRootPath = path.join(__dirname, "../../bpay-front");
+  const frontSrcPath = path.join(frontRootPath, "/src");
+  const frontAbiFolderPath = path.join(frontSrcPath, "/abi");
 
   fs.writeFileSync(
-    `C:/Users/Sumba/OneDrive/Desktop/BPay/bpay/src/abi/${SUB_CONTRACT_NAME}.json`,
-    JSON.stringify(subscriptionServiceAbi)
+    path.join(frontAbiFolderPath, `${SUB_CONTRACT_NAME}.ts`),
+    `export const ${SUB_CONTRACT_NAME}Abi = ${JSON.stringify(
+      subscriptionServiceAbi
+    )} as const`
   );
 
   fs.writeFileSync(
-    `C:/Users/Sumba/OneDrive/Desktop/BPay/bpay/src/abi/${BPAY_CONTRACT_NAME}.json`,
-    JSON.stringify(bpayAbi)
+    path.join(frontAbiFolderPath, `${BPAY_CONTRACT_NAME}.ts`),
+    `export const ${BPAY_CONTRACT_NAME}Abi = ${JSON.stringify(bpayAbi)} as const`
+  );
+
+  const subAddress = await subscriptionService.getAddress();
+  const tokenAddress = await bpayToken.getAddress();
+
+  await Promise.all(
+    users.map((user) => {
+      return bpayToken
+        .connect(owner)
+        .mint(user.address)
+        .then((tx) => tx.wait());
+    })
   );
 
   const fileContent = `
-  export const SUBSCRIPTION_CONTRACT_ADDRESS = "${await subscriptionService.getAddress()}";
-  export const BPAY_MONEY_CONTRACT_ADDRESS = "${await bpayToken.getAddress()}";
+  export const SUBSCRIPTION_CONTRACT_ADDRESS = "${subAddress}";
+  export const BPAY_MONEY_CONTRACT_ADDRESS = "${tokenAddress}";
 `;
 
-  fs.writeFileSync(
-    "C:/Users/Sumba/OneDrive/Desktop/BPay/bpay/src/contract.ts",
-    fileContent
-  );
+  fs.writeFileSync(path.join(frontSrcPath, "/contract.ts"), fileContent);
+  // @ts-ignore
+  exec("yarn generate", { cwd: frontRootPath }, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
