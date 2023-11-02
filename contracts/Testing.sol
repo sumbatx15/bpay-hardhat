fd277b6c1fab9e3262fd381a663e421a18157f37
sumbatx15 <sumbat15@gmail.com>
2023-11-03 01:54:20 +0200

    x

--- contracts/Testing.sol
+++ contracts/Testing.sol
@@ -7,15 +7,17 @@ import "hardhat/console.sol";
 
 contract Testing is Ownable {
     struct Plan {
+        uint256 id;
         address merchant;
         string name;
+        IERC20 token;
         uint256 price;
         uint256 period;
     }
 
     struct Subscription {
         uint256 id;
-        address customerAddress; // Added customerAddress field
+        address customer;
         uint256 createdAt;
         uint256 updatedAt;
     }
@@ -23,25 +25,62 @@ contract Testing is Ownable {
     constructor(address _initialOwner) Ownable(_initialOwner) {}
 
     Plan[] public plans;
+    mapping(uint256 => Subscription[]) public subscriptionsByPlanId;
 
-    uint256 private planCounter;
+    uint256 private planCounter = 0;
     uint256 private subscriptionCounter;
-    mapping(uint256 => Plan) private plansMap;
 
     function createPlan(
-        string memory _name,
+        string calldata _name,
+        address _token,
         uint256 _price,
         uint256 _period
     ) external {
-        plansMap[planCounter++] = Plan({
-            merchant: msg.sender,
-            name: _name,
-            price: _price,
-            period: _period
-        });
+        plans.push(
+            Plan({
+                id: plans.length,
+                merchant: msg.sender,
+                name: _name,
+                token: IERC20(_token),
+                price: _price,
+                period: _period
+            })
+        );
+    }
+
+    function removePlan(uint256 _planId) external {
+        require(
+            plans[_planId].merchant == msg.sender,
+            "Only the merchant can remove this plan"
+        );
+        delete plans[_planId];
     }
 
     function getPlans() external view returns (Plan[] memory) {
         return plans;
+    }
+
+    function getPlanById(uint256 _planId) external view returns (Plan memory) {
+        return plans[_planId];
+    }
+
+    function execute(uint256 _planId, uint256[] calldata _subIndexes) external {
+        Plan memory plan = plans[_planId];
+        require(
+            plan.merchant == msg.sender,
+            "Only the merchant can execute this plan"
+        );
+        for (uint256 i = 0; i < _subIndexes.length; i++) {
+            Subscription memory sub = subscriptionsByPlanId[_planId][
+                _subIndexes[i]
+            ];
+            require(sub.customer != address(0), "Subscription does not exist");
+            require(
+                sub.updatedAt + plan.period < block.timestamp,
+                "Subscription is not due"
+            );
+            sub.updatedAt = block.timestamp;
+            subscriptionsByPlanId[_planId][_subIndexes[i]] = sub;
+        }
     }
 }
--- test/Testing.ts
+++ test/Testing.ts
@@ -12,12 +12,18 @@ describe("Testing", function () {
     return await SubscriptionService.deploy(owner.address);
   };
 
-  const addPlan = async (account: HardhatEthersSigner) =>
+  const addPlan = async (account: HardhatEthersSigner, name: string) =>
     await contract
       .connect(account)
-      .createPlan("Dani", ethers.parseEther("19.99"), 1, {
-        gasPrice: ethers.parseUnits("15", "gwei"),
-      })
+      .createPlan(
+        name,
+        "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+        ethers.parseEther("19.99"),
+        1,
+        {
+          gasPrice: ethers.parseUnits("15", "gwei"),
+        }
+      )
       .then((tx) => tx.wait())
       .then((tx) => {
         console.log(tx?.gasUsed);
@@ -34,12 +40,40 @@ describe("Testing", function () {
     await Promise.all(
       Array(5)
         .fill(0)
-        .map(() => addPlan(account))
+        .map((_, i) => addPlan(account, `Plan ${i}`))
     );
+    await contract
+      .connect(account)
+      .removePlan(2)
+      .then((tx) => tx.wait())
+      .then((tx) => {
+        console.log("remove", tx?.gasUsed);
+        return tx;
+      });
+
     await Promise.all(
       Array(5)
         .fill(0)
-        .map(() => addPlan(account2))
+        .map((_, i) => addPlan(account2, `Plan2 ${i}`))
     );
+
+    const plan = await contract.getPlanById(1);
+    expect(plan.name).to.equal("Plan 1");
+
+    const plans = (await contract.getPlans()).reduce((acc, plan) => {
+      const merchant = plan.merchant;
+      if (!acc[merchant]) {
+        acc[merchant] = [];
+      }
+      acc[merchant].push({
+        id: plan.id,
+        name: plan.name,
+        price: plan.price.toString(),
+        period: plan.period.toString(),
+        token: plan.token,
+      });
+      return acc;
+    }, {} as Record<string, any>);
+    console.log('plans:', plans)
   });
 });
